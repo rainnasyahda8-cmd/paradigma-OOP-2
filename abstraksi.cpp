0733649f1d50e43fc3e13910d3324af26aa2504d
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-18 14:48:12 +0700

    menambahkan void

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    void
+}
