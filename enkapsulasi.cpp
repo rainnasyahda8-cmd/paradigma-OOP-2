2e00456744af96b201f0b50a84976425e472476e
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-18 14:30:43 +0700

    menambahhkan private

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 class remoteLampu 
 {
-    
+private:
+    string saklarNo[5];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remote
 }
