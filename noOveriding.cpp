40a3239d28344d19a8b2fdf626f0952d27314121
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-18 14:38:05 +0700

    menambahkan class

--- noOveriding.cpp
+++ noOveriding.cpp
@@ -4,6 +4,9 @@ using namespace std;
 class baseClass {
 public:
     virtual void perkenalan() final{
-        
+        cout << "Hallo saya Function dari base class";
     }
+};
+class derivedClass : public baseClass {
+    
 }
