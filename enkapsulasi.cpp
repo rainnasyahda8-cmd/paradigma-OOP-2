42d99482091301b941ccc5d6e1ce1126459aba9a
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-18 14:33:13 +0700

    menambahkan cout

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -16,4 +16,12 @@ public:
 
 int main() {
     remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(0) << endl;
 }
